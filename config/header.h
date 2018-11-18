3c0533cf573a54f84a9838ff6ac5990322dcc96e
MatheusrSantos <matheus.r.d.s10@hotmail.com>
2018-11-18 00:46:00 -0200

    Nova palavra reservada adicionada

--- config/header.h
+++ config/header.h
@@ -6,12 +6,12 @@
 #include <ctype.h>
 #include <sys/stat.h>
 #include <sys/types.h>
-#define RESERVED_WORDS_SIZE 16
+#define RESERVED_WORDS_SIZE 17
 #define RESERVED_SYMBOLS_SIZE 4
 #define RESERVED_PRIMITIVE_TYPES_SIZE 4
 #define RESERVED_FILES_NAME_SIZE 1
 // reserved words, symbols and types
-char* reserved_words[RESERVED_WORDS_SIZE] = {"help","create", "delete", "select", "set", "list", "insert", "into", "add", "table", "tables", "columns","from", "where", "by", "values"};
+char* reserved_words[RESERVED_WORDS_SIZE] = {"help","create", "delete", "select", "set", "list", "insert", "into", "add", "table", "tables", "columns","from", "where", "by", "values", "and"};
 char reserved_symbols[RESERVED_SYMBOLS_SIZE] = {'=', '>', '<', '*'};
 char* primitive_types[RESERVED_PRIMITIVE_TYPES_SIZE] = {"int", "float", "char", "date"};
 char* reserved_files_name[RESERVED_FILES_NAME_SIZE] = {"default_db.csv"};
